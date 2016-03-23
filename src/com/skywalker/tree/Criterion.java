35f38a9d8cf8b2b87b45857d97a96989d4bec0a9
Cao Nannan <caonn@mvad.com>
2016-03-23 22:44:49 +0800

    revise

--- src/com/skywalker/tree/Criterion.java
+++ src/com/skywalker/tree/Criterion.java
@@ -3,6 +3,9 @@ package com.skywalker.tree;
 import com.skywalker.utils.Tuple;
 import org.jblas.DoubleMatrix;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The criterion can only be used for two classes.
  * For multiple class case, it will be implemented later.
@@ -40,8 +43,7 @@ class MseCriterion extends Criterion {
   public double getCriterionValue(int index) {
     updateSum(index);
     updateAvg(index);
-
-    return 0;
+    return updateCriterion();
   }
 
   protected  void updateSum( int index ) {
@@ -55,6 +57,19 @@ class MseCriterion extends Criterion {
     avgPrefix = sumPrefix / index;
     avgSuffix = sumSuffix / ( size - index - 1);
   }
+
+  protected double updateCriterion() {
+    double criterionValue = 0;
+    for(int i = 0; i < lastPointer; i++) {
+      double value = y.get(indices[i]) - avgPrefix;
+      criterionValue += value * value;
+    }
+    for(int i = lastPointer + 1; i < size; i++) {
+      double value = y.get(indices[i]) - avgSuffix;
+      criterionValue += value * value;
+    }
+    return criterionValue;
+  }
 }
 
 class GiniCriterion extends Criterion {
@@ -65,9 +80,34 @@ class GiniCriterion extends Criterion {
 }
 
 class MissClassCriterion extends Criterion {
+  private double p = 0;
+  private int numLabel = 0;
+  private int [] labelArray;
+
   @Override
-  public double getCriterionValue(int i) {
-    return 0;
+  public void init(Tuple<Double, Integer>[] arrIndices ) {
+    super.init(arrIndices);
+    List<Integer> labelList = new ArrayList<Integer>();
+    double preValue = arrIndices[0].first();
+    int labelCounter = 1;
+    for( int i = 1; i < size; i++ ) {
+      double nextValue = arrIndices[i].first();
+      if( Math.abs(preValue - nextValue) > 0.001 ) {
+        labelList.add(labelCounter);
+        labelCounter = 0;
+        preValue = nextValue;
+      }
+      labelCounter += 1;
+    }
   }
+  @Override
+  public double getCriterionValue(int index) {
+    updateP(index);
+    return p * (1-p);
+  }
+
+  protected void updateP( int index ) {
+  }
+
 }
 
