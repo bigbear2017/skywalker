9443850e86a0adbe1afc79155e4b4dea6f7dae6d
Cao Nannan <caonn@mvad.com>
2016-03-23 08:37:04 +0800

    reformat code

--- src/com/skywalker/tree/Criterion.java
+++ src/com/skywalker/tree/Criterion.java
@@ -6,23 +6,26 @@ import org.jblas.DoubleMatrix;
 /**
  * The criterion can only be used for two classes.
  * For multiple class case, it will be implemented later.
+ *
  * @author caonn
  * @version 16-3-7.
  */
 public abstract class Criterion {
-  protected double [] data;
-  protected int [] indices;
-  protected DoubleMatrix y ;
-  public void init(Tuple<Double, Integer>[] arrIndices){
+  protected double[] data;
+  protected int[] indices;
+  protected DoubleMatrix y;
+
+  public void init(Tuple<Double, Integer>[] arrIndices) {
     int size = arrIndices.length;
     data = new double[size];
-    indices = new int [size];
-    for( int i = 0; i < size; i++ ) {
+    indices = new int[size];
+    for (int i = 0; i < size; i++) {
       data[i] = arrIndices[i].first();
       indices[i] = arrIndices[i].second();
     }
   }
-  public abstract  double getCriterionValue(int i);
+
+  public abstract double getCriterionValue(int i);
 }
 
 class MseCriterion extends Criterion {
--- src/com/skywalker/tree/Node.java
+++ src/com/skywalker/tree/Node.java
@@ -7,26 +7,28 @@ import org.jblas.DoubleMatrix;
 import java.util.HashMap;
 import java.util.Map;
 
-/** This class define the node of decision tree.
+/**
+ * This class define the node of decision tree.
+ *
  * @author caonn
  * @version 16-3-4.
  */
 public class Node {
+  private static DoubleMatrix x;
+  private static DoubleMatrix y;
+  private static int featureSize;
+  private static Criterion criterion = null;
+  private static Map<String, Criterion> criterionMap = new HashMap<String, Criterion>();
+
+  static {
+    criterionMap.put("mse", new MseCriterion());
+  }
+
   private Node leftNode = null; //Left Tree to traversal
   private Node rightNode = null; //Right Tree to traversal
   private int featureIndex;
   private double featureValue;
   private int[] indices; //all the data points that belongs to this node
-
-  private static DoubleMatrix x;
-  private static DoubleMatrix y;
-  private static int featureSize;
-  private static Criterion criterion = null;
-
-  private static Map<String, Criterion>  criterionMap = new HashMap<String, Criterion>();
-  static {
-    criterionMap.put("mse", new MseCriterion());
-  }
 
   public Node(int[] indices) {
     this.leftNode = null;
@@ -36,24 +38,7 @@ public class Node {
     this.indices = indices;
   }
 
-  public double getLabel(DoubleMatrix xp) {
-    double res = 0;
-
-    return res;
-  }
-
   public Node() {
-  }
-
-  public boolean isLeafNode() {
-    if(leftNode == null && rightNode == null) {
-      return true;
-    }
-    return false;
-  }
-
-  public double getLabel() {
-    return 0;
   }
 
   public static Node getHeadNode(DoubleMatrix x, DoubleMatrix y, int numSamples, int featureSize, String criterion) {
@@ -64,21 +49,38 @@ public class Node {
     return new Node(new int[numSamples]);
   }
 
+  public double getLabel(DoubleMatrix xp) {
+    double res = 0;
+
+    return res;
+  }
+
+  public boolean isLeafNode() {
+    if (leftNode == null && rightNode == null) {
+      return true;
+    }
+    return false;
+  }
+
+  public double getLabel() {
+    return 0;
+  }
+
   public Splitter getBestSplitter() {
     Splitter bestSplitter = new Splitter();
     double bestCriterion = Double.MIN_VALUE;
-    for(int f = 0; f < featureSize; f++) {
+    for (int f = 0; f < featureSize; f++) {
       DoubleMatrix feature = x.getColumns(indices).getColumn(f);//for the first feature, get the split point.
-      double [] data = feature.data;
+      double[] data = feature.data;
       Tuple<Double, Integer>[] arrIndices = Sorter.sortDoubleArrayWithIndex(data);
       criterion.init(arrIndices);
       int arrSize = arrIndices.length;
-      for( int i = 0; i < arrSize; i++ ) {
-        while( (i < arrSize - 1) && (arrIndices[i].first() != arrIndices[i+1].first())  ) {
+      for (int i = 0; i < arrSize; i++) {
+        while ((i < arrSize - 1) && (arrIndices[i].first() != arrIndices[i + 1].first())) {
           i++;
         }
         double criterionValue = criterion.getCriterionValue(i);
-        if( criterionValue > bestCriterion) {
+        if (criterionValue > bestCriterion) {
           bestSplitter.setFeatureIndex(f);
           bestSplitter.setFeatureValue(arrIndices[i].first());
         }
