1be52763eecdf435f209768358b638b332a02c5d
Michael Cao <caonn@mvad.com>
2016-06-14 10:49:53 +0800

    fix isotonic regression

--- src/main/java/com/skywalker/linear/IsonicRegression.java
+++ src/main/java/com/skywalker/linear/IsonicRegression.java
@@ -5,6 +5,7 @@ import com.skywalker.utils.Sorter;
 import com.skywalker.utils.Tuple;
 
 import java.util.List;
+import java.util.Stack;
 
 /**
  * This class implements the PAVA algorithm.
@@ -12,34 +13,69 @@ import java.util.List;
  * @version 16/6/12.
  */
 public class IsonicRegression {
-  private List<Record> records = Lists.newArrayList();
+  private Stack<Record> records = new Stack<Record>();
 
   /**
    * This function will fit the data points according to pava algorithm.
    * @param tuples the input points, tuple.first is index, tuple.second is value
-   * @param <T1> the type of index, could be int or double
-   * @param <T2> the type of value, could be int or double
    */
-  public <T1 extends Comparable< ? super T1>, T2 > void fit(List<Tuple<T1 ,T2>> tuples) {
+  public  void fit (List<Tuple<Double, Double>> tuples) {
     Sorter.sortTupleList(tuples);
-    for( Tuple t : tuples) {
-      Record record = new Record( (Comparable)t.first(), (Comparable)t.first(), t.second());
+    for( Tuple<Double, Double> t : tuples) {
+      Record record = new Record( t.first(), t.first(), t.second(), 1);
+      merge(record);
     }
   }
 
-  public <T1, T2> List<T2> predict(List<Tuple<T1, T2>> tuples) {
+  /**
+   * merge record to record list.
+   * @param record record to be merged into records.
+   */
+  private void merge(Record record) {
+    if( records.size() == 0 ) {
+      records.add(record);
+    }
+    Record lastRecord = records.peek();
+    if( lastRecord.value <= record.value ) {
+      records.add(record);
+    } else {
+      records.pop();
+      int newNumber = record.number + lastRecord.number;
+      Double newValue = ( record.number * record.value + lastRecord.number * lastRecord.value ) / newNumber;
+      Record newRecord = new Record(lastRecord.lowIndex, record.highIndex, newValue, newNumber);
+      merge(newRecord);
+    }
+
+  }
+
+  public void printRecords() {
+    for (Record record : records) {
+      System.out.println(record.toString());
+    }
+  }
+
+  public List<Double> predict(List<Tuple<Double, Double>> tuples) {
 
     return Lists.newArrayList();
   }
+
+  public static void main(String [] args) {
+
+  }
 }
 
-class Record<T1 extends Comparable<? super T1>, T2> {
-  public final T1 lowIndex;
-  public final T1 highIndex;
-  public final T2 value;
-  public Record(T1 lowIndex, T1 highIndex, T2 value) {
+class Record {
+  public final double lowIndex;
+  public final double highIndex;
+  public final double value;
+  public final int number;
+  public Record(double lowIndex, double highIndex, double value, int number) {
     this.lowIndex = lowIndex;
     this.highIndex = highIndex;
     this.value = value;
+    this.number = number;
+  }
+  public String toString() {
+    return String.valueOf(lowIndex) + " " + String.valueOf(highIndex) + " " + String.valueOf(value);
   }
 }
